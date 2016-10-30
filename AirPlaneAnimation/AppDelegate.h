d303e2056365d2d5fe7ab85b6658490a90777eff
Bear <648070256@qq.com>
2016-10-30 15:12:46 +0800

    first commit

---
+++ AirPlaneAnimation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AirPlaneAnimation
+//
+//  Created by apple on 16/10/30.
+//  Copyright © 2016年 Bear. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
